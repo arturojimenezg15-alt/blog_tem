a0d2460e9661b89f55ad1204aac7e507c77ddd86
Lr1808 <rodulfoluis1808@gmail.com>
2025-11-01 12:03:28 -0700

    create view

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import PublicationListView, PublicationCreteView
+from .views import (
+                    PublicationListView, 
+                    PublicationCreteView,
+                    PublicationDetailView
+                  )                   
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='Publications-list'),
     path('publication/new/', PublicationCreteView.as_view(), name='Publication-create'),
+    path('publication/<int:pk>/', PublicationDetailView.as_view(), name='Publication-detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Publication
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 # Create your views here.
 
@@ -13,4 +13,8 @@ class PublicationCreteView(CreateView):
     model = Publication
     template_name = 'create.html'
     fields = ['title', 'content', 'author']
+
+class PublicationDetailView(DetailView):
+    model = Publication
+    template_name = 'publication_detail.html'    
     
