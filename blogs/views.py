6e9f396b08e3a48fad9e0209b1eadf746bb74df7
arturojimenezg15 <arturojimenezg15@gmail.com>
2025-11-29 10:12:58 -0400

    permisos agregados en detail y create

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from .models import Publication
 from django.views.generic import ListView, DetailView
@@ -10,13 +11,13 @@ class PublicationListView(ListView):
     model = Publication
     template_name = 'home.html'
 
-class PublicationCreteView(CreateView):
+class PublicationCreteView(LoginRequiredMixin, CreateView):
     model = Publication
     template_name = 'create.html'
     fields = ['title', 'content', 'author']
     success_url = reverse_lazy('publications-list')
 
-class PublicationDetailView(DetailView):
+class PublicationDetailView(LoginRequiredMixin, DetailView):
     model = Publication
     template_name = 'publication_detail.html'    
     
